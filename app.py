4771f4fe18f17872ffc54236f0cab5bb64dce46c
OPTIMISTICLE <bibalefai@gmail.com>
2024-05-13 09:04:02 +0000

    working one

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import *
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 try:
-    db_path = 'mysql+pymysql://root:root@localhost:3306/savemydata'  # Modifier le chemin de la base de données si nécessaire
-    engine = create_engine(db_path)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    client = MongoClient('mongodb://localhost:27017/')  # Connect to MongoDB server
+    db = client.savemydata  # Select database
+    collection = db.data  # Select collection
 except Exception as e:
     print(e)
+
 @app.route('/save', methods=["POST"])
 def save():
-
     valeur = request.json['valeur']
 
-    new_save = Data(valeur)
-    session.add(new_save)
-    session.commit()
+    new_save = {"valeur": valeur}
+    result = collection.insert_one(new_save)
 
-    saved_data = session.query(Data).get(new_save.id)
+    saved_data = collection.find_one({"_id": result.inserted_id})
 
-    return jsonify({"id": saved_data.id})
+    return jsonify({"id": str(saved_data["_id"])})
 
 @app.route('/hello')
 def hello_world():
     return "hello world"
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
